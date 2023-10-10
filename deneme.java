940b7dc209af12a0fbc8badf205b238d9de80597
yusufnrn <yusufnarinn@outlook.com>
2023-10-10 09:41:56 +0300

    1

--- deneme.java
+++ deneme.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class deneme {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Merhaba!");
     }
 }
