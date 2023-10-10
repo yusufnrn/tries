980243395ea4a0f6a7c1ae2fd1b45907ad39a70c
yusufnrn <yusufnarinn@outlook.com>
2023-10-10 10:27:23 +0300

    hm

--- deneme.java
+++ deneme.java
@@ -4,5 +4,40 @@ public class deneme {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Merhaba!");
-    }
+        System.out.println("Bir islem seciniz!!");
+        String string = new String ("\n1. Toplama" +
+                                   "\n2. Cikarma" +
+                                   "\n3. Carpma" +
+                                   "\n4. Bolme");
+        System.out.println(string);
+        int i = scanner.nextInt();
+        if(i == 1){
+            System.out.println("Toplanacak iki sayi giriniz: ");
+            int number1 = scanner.nextInt();
+            int number2 = scanner.nextInt();
+            int sonuc = number1 + number2;
+            System.out.println("Sayilarin Toplami: " + sonuc );
+        }
+        else if (i == 2) {
+            System.out.println("Cikarilacak iki sayi giriniz: ");
+            int number1 = scanner.nextInt();
+            int number2 = scanner.nextInt();
+            int sonuc = number1 - number2;
+            System.out.println("Sayilarin Farki: " + sonuc);
+        }
+             else if (i == 3) {
+                System.out.println("Carpilacak iki sayi giriniz: ");
+                int number1 = scanner.nextInt();
+                int number2 = scanner.nextInt();
+                int sonuc = number1 * number2;
+                System.out.println("Sayilarin Carpimi: " + sonuc);
+            }
+        else if (i == 4) {
+            System.out.println("Bolunecek sayilari giriniz: ");
+            int number1 = scanner.nextInt();
+            int number2 = scanner.nextInt();
+            int sonuc = number1 / number2;
+            System.out.println("Sayilarin Bolumu: " + sonuc);
+            }
+        }
 }
